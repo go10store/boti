b47219808e093fba79eecd7a16d587c6beb961f7
go10store <go10store@protonmail.com>
2025-12-09 06:06:54 +0200

    Update security.py

--- security.py
+++ security.py
@@ -14,8 +14,8 @@ def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
 def get_password_hash(password):
-    # Bcrypt has a 72 byte limit
-    return pwd_context.hash(password[:72])
+    # Bcrypt has a 72 byte limit, use 50 chars to be safe with unicode
+    return pwd_context.hash(password[:50])
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
