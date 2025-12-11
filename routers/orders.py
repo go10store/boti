288d1994687e927a48df70a8866e90d5fc2de6c0
nono <go10store@protonmail.com>
2025-12-12 00:47:44 +0200

    Update orders.py

--- routers/orders.py
+++ routers/orders.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from typing import List
+from typing import List, Optional
 import models, schemas
 from database import get_db
 from routers.auth import get_current_user
